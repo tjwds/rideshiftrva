bcd8aaa925797e60f3582778927313ee61fdabbc
gareth bk <garethbk@gmail.com>
2026-03-28 17:16:49 -0400

    Fix conflict

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,9 +4,5 @@ import { routing } from "./i18n/routing";
 export default createMiddleware(routing);
 
 export const config = {
-<<<<<<< HEAD
-  matcher: ["/", "/(en|es)/:path*", "/admin/:path*"],
-=======
   matcher: ["/((?!api|_next|.*\\..*).*)"],
->>>>>>> 75bea018c27a2c0bae5e1583e7c5d00233428071
 };
